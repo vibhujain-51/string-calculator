da92e95c9458477b4933acd20f3abc3326e1c7d4
Vibhu Jain <vibhu.jain@refactor.academy>
2024-10-19 19:00:38 +0530

    updated function to accept any any amount of number also accept multiple digit numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,3 +1,7 @@
+// Test Case 1:
+// Input: ""
+// Output: 0
+
 // Test Case 2:
 // Input: "1"
 // Output: 1
@@ -10,19 +14,51 @@
 // Input: "        1,               5      "
 // Output: 6
 
+// Test Case 5:
+// Input: "1, 5, 4, 7, 2, 6, 9"
+// Output: 34
+
+// Test Case 6:
+// Input: "1, 5, 4, 7, 2, 6, 9   "
+// Output: 34
+
+// Test Case 7:
+// Input: "100 , 5, 4, 7, 2, 6, 9   "
+// Output: 34
+
 function add(dataString) {
   if (!!dataString) {
     // This block will be executed if dataString is not empty
 
-    if (dataString.length === 1) {
-      return parseInt(dataString);
-    } else {
-      const newDataString = dataString.split(",").join("").split(" ").join("");
-      return parseInt(newDataString[0]) + parseInt(newDataString[1]);
+    let sum = 0;
+
+    // 1. Sanitize the string of unnecessary white spaces
+    const cleanedStringArray = sanitizeString(dataString);
+
+    // 2. Perform the addition
+
+    if (cleanedStringArray.length === 1) {
+      return parseInt(cleanedStringArray[0]);
     }
+
+    cleanedStringArray.forEach((str) => {
+      sum += parseInt(str);
+    });
+
+    return sum;
   }
 
   return 0;
 }
 
-console.log(add("1,5"));
+function sanitizeString(data) {
+  const cleanedString = data.split(",").map((str) => {
+    if (!!str) {
+      return str.split(" ").join("").trim();
+    }
+  });
+
+  return cleanedString;
+}
+
+console.log(add("100 , 5, 4, 7, 2, 6, 9   "));
