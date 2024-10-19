a08ebcac29708f1d58c4b144fb478d5a274a97f2
Vibhu Jain <vibhu.jain@refactor.academy>
2024-10-19 18:11:19 +0530

    updated function to perform calculation on string with 1 number and 2 numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,15 +1,28 @@
-// Test Case 1:
-// Input: Empty String, null string or undefined string
-// Output: 0
+// Test Case 2:
+// Input: "1"
+// Output: 1
+
+// Test Case 3:
+// Input: "1,5"
+// Output: 6
+
+// Test Case 4:
+// Input: "        1,               5      "
+// Output: 6
 
 function add(dataString) {
   if (!!dataString) {
     // This block will be executed if dataString is not empty
 
-    return "sum of string";
+    if (dataString.length === 1) {
+      return parseInt(dataString);
+    } else {
+      const newDataString = dataString.split(",").join("").split(" ").join("");
+      return parseInt(newDataString[0]) + parseInt(newDataString[1]);
+    }
   }
 
   return 0;
 }
 
-console.log(add(""));
+console.log(add("1,5"));
